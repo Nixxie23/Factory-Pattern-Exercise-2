c09999f0f7f938dd331214b15fd9cc519f832cb3
taylor Schuchter <taylorschuchter@gmail.com>
2023-02-20 19:30:10 -0500

    Project complete

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace FactoryPatternExercise2
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What database would you like to use? A list, SQL, or Mongo?");
+            var userResponse = Console.ReadLine();
+            IDataAccess accessType = DataAccessFactory.GetDataAccessType(userResponse);
+            
+                accessType.SaveData();
+                var  productList = accessType.LoadData();
+            
+            Product soap = new Product() { Name = "Dawn", Price = 1.55 };
+            Product medicine = new Product() { Name = "Tylenol", Price = 2.99 };
+            productList.Add(soap);
+            productList.Add(medicine);
+            foreach(var item in productList) 
+            {
+                Console.WriteLine($"{item.Name} is ${item.Price}");
+            }
+
         }
     }
 }
+
+
